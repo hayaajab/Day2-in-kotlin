ad73737c001fc9b3d6865c1b13f3dba7a10ad7b8
Haya <52868771+hayaajab@users.noreply.github.com>
2023-01-03 11:21:47 +0300

    Add files via upload

---
+++ Assignment_2.kt
@@ -0,0 +1,20 @@
+@file:Suppress("UNREACHABLE_CODE")
+
+package com.example.day2
+
+
+fun main() {
+    for (i in 0.. 100){
+        println(i)
+
+        if (i ==71) {
+            println( "IT'S OVER 70!!!")
+            break
+        }
+
+
+
+    }
+}
+
+
