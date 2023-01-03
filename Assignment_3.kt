c6ed74009657047b7070030958f3c4a55778f5df
Haya <52868771+hayaajab@users.noreply.github.com>
2023-01-03 16:25:57 +0300

    Update Assignment_3.kt

--- Assignment_3.kt
+++ Assignment_3.kt
@@ -5,16 +5,15 @@ package com.example.day2
 fun main() {
     var Temperature = 35
 
-    while (Temperature <= 35) {
-        Temperature --
+    while (Temperature > 10 ) {
+
         when{
             Temperature == 30 -> println("$Temperature It's Hot")
             Temperature == 20 -> println("$Temperature It's comfy")
             Temperature == 15 -> println("$Temperature It's cold!")
-            else->continue
-
+            else-> println(Temperature)
 
         }
-
+        Temperature --
     }
 }
